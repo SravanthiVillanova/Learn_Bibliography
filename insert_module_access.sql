80fbdae9c1dc728deef1025d839d322315ca4e91
SravanthiVillanova <sadusumi@villanova.edu>
2017-08-02 09:53:06 -0400

    Removed unnecessary data files

--- insert_module_access.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `module_access`(`id`, `module`, `role_a`, `role_su`, `role_u`) VALUES (1,'Work','yes','yes','yes');
-INSERT INTO `module_access`(`id`, `module`, `role_a`, `role_su`, `role_u`) VALUES (2,'Agent','yes','yes','yes');
-INSERT INTO `module_access`(`id`, `module`, `role_a`, `role_su`, `role_u`) VALUES (3,'Publisher','yes','yes','yes');
-INSERT INTO `module_access`(`id`, `module`, `role_a`, `role_su`, `role_u`) VALUES (4,'Preferences','yes','yes','yes');
-INSERT INTO `module_access`(`id`, `module`, `role_a`, `role_su`, `role_u`) VALUES (5,'Classification','yes','yes','no');
-INSERT INTO `module_access`(`id`, `module`, `role_a`, `role_su`, `role_u`) VALUES (6,'Work Type','yes','no','no');
-INSERT INTO `module_access`(`id`, `module`, `role_a`, `role_su`, `role_u`) VALUES (7,'Agent Type','yes','no','no');
-INSERT INTO `module_access`(`id`, `module`, `role_a`, `role_su`, `role_u`) VALUES (8,'Language','yes','no','no');
-INSERT INTO `module_access`(`id`, `module`, `role_a`, `role_su`, `role_u`) VALUES (9,'Users','yes','no','no');
